0cec63c0cfb58a4b670e599e1785e6036c5d4a8d
aziscript <waltneku@gmail.com>
2025-05-13 10:53:04 +0100

    Updated queries

--- Introduction to SQL/intro_queries.sql
+++ Introduction to SQL/intro_queries.sql
@@ -1,2 +1,11 @@
-SELECT title, author, "year", genre
+-- Return all titles from the books table
+SELECT title
+FROM books;
+
+-- Select title and author from the books table
+SELECT title, author
+FROM books;
+
+-- Select all fields from the books table
+SELECT *
 FROM books;
