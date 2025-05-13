8df1efc32035d6c55d5dfc18e45e730916cdd24e
aziscript <waltneku@gmail.com>
2025-05-13 19:26:40 +0100

    More query

---
+++ Intermediate SQL/interm_reviews.sql
@@ -0,0 +1,14 @@
+select *
+from reviews
+limit 10;
+
+-- Select film_id and imdb_score with an imdb_score over 7.0
+SELECT film_id, imbd_score
+FROM reviews
+WHERE imbd_score > 7.0;
+
+-- Select film_id and facebook_likes for ten records with less than 1000 likes 
+SELECT film_id, facebook_likes
+FROM reviews
+WHERE facebook_likes < 1000
+LIMIT 10;
