3a29c128b09d2f479e9b741e13e344fbe351fb10
aziscript <waltneku@gmail.com>
2025-05-14 09:39:13 +0100

    Updated

--- Intermediate SQL/interm_films.sql
+++ Intermediate SQL/interm_films.sql
@@ -77,3 +77,55 @@ WHERE (release_year = 1990 OR release_year = 1999)
 	AND (language = 'English' OR language = 'Spanish')
 -- Filter films with more than $2,000,000 gross
 	AND gross > 2000000;
+
+-- Select the title and release_year for films released between 1990 and 2000
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000;
+
+SELECT title, release_year
+FROM films
+WHERE (release_year BETWEEN 1990 AND 2000)
+-- Narrow down your query to films with budgets > $100 million
+	AND budget > 100000000;
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Restrict the query to only Spanish-language films
+	AND language = 'Spanish';
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Amend the query to include Spanish or French-language films
+	AND (language='Spanish' OR language='French');
+
+-- Find the title and release_year for all films over two hours in length released in 1990 and 2000
+SELECT title, release_year
+FROM films
+WHERE release_year IN (1990, 2000)
+  AND duration > 120;
+
+-- Find the title and language of all films in English, Spanish, and French
+SELECT title, language
+FROM films
+WHERE language IN ('English', 'Spanish', 'French');
+
+-- Find the title, certification, and language all films certified NC-17 or R that are in English, Italian, or Greek
+SELECT title, certification, language
+FROM films
+WHERE certification IN ('NC-17', 'R')
+  AND language IN ('English', 'Italian', 'Greek');
+
+-- Count the unique titles
+SELECT COUNT(DISTINCT title) AS nineties_english_films_for_teens
+FROM films
+-- Filter to release_years to between 1990 and 1999
+WHERE release_year BETWEEN 1990 AND 1999
+-- Filter to English-language films
+	AND language = 'English'
+-- Narrow it down to G, PG, and PG-13 certifications
+	AND certification IN ('G', 'PG', 'PG-13');
